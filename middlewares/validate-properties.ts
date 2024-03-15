973eb8acf34c84ad11fad84c745b06c92bbcdd9c
luislunav <luis.a.luna.v@outlook.com>
2024-03-15 00:28:32 -0600

    Se agregan los validadores para validar existancia de usuario y correo

--- middlewares/validate-properties.ts
+++ middlewares/validate-properties.ts
@@ -1,16 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
-import { validationResult } from 'express-validator';
+import { validationResult, Result } from 'express-validator';
 
-const validateProperties=( req: Request, res: Response, next: NextFunction )=>{
-    const errors = validationResult( req );
+const validateProperties = (req: Request, res: Response, next: NextFunction) => {
+    const errors: Result = validationResult(req);
 
-    if( !errors.isEmpty() ){
-        res.status(400).json( errors )
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors);
     }
 
-    next();
-}
+    next(); // Llama a next() solo si no hay errores
+};
 
-export{
-    validateProperties
-}
+export { validateProperties };
