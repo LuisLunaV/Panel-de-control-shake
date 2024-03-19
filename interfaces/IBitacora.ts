17dbf80b2e5ceeb486a02ebad9c9efc885ed8339
luislunav <luis.a.luna.v@outlook.com>
2024-03-19 15:38:07 -0600

    Creamos la interface y el modelo de la bitacora de registro

---
+++ interfaces/IBitacora.ts
@@ -0,0 +1,7 @@
+export interface IBitacora{
+    Bitacora_Id?: number,
+    Bitacora_NumEmpleado: string,
+    Bitacora_Imagen:string,
+    Bitacora_Movimiento:string
+    // Bitacora_Fecha:string
+}
